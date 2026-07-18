c70fea99e655536862b206ebd28b5c0bb78f5e2c
hithaishi <hithaishial2007@gmail.com>
2026-07-18 11:53:10 +0530

    sum of array elements

---
+++ DSA problems/sum_of_array_elements.js
@@ -0,0 +1,9 @@
+let evenNumbers = [2, 4, 6, 8, 10, 12]
+let sum = 0;
+
+for(let i = 0; i< evenNumbers.length; i++){
+
+    sum += evenNumbers[i] ;
+}
+
+console.log("the sum of array elements is: ", sum);
